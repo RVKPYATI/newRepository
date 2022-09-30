e04e82ecc118f94de1ce9fa1806843fd9c0f29ac
Vasiliy Rusanov <rvk1986@mail.ru>
2022-09-30 08:14:18 +0300

    Completed second lesson

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-const title = '';
-const screens = '';
-const screenPrice = '';
-const rollback = '';
-const fullPrice = 0;
-const adaptive = '';
+const title = 'Мой первый проект';
+const screens = 'Простые, Сложные, Интерактивные';
+const screenPrice = 15;
+const rollback = 56;
+const fullPrice = 2000;
+const adaptive = true;
 
-alert('Любой текст');
-console.log('Любой текст');
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log(`"Стоимость верстки экранов ${screenPrice} долларов" и “Стоимость разработки сайта ${fullPrice})долларов`);
+
+console.log(screens.toLocaleLowerCase().split(', '));
+
+console.log((fullPrice * (rollback/100)));
