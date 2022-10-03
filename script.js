e42732a6e791beced674014fe82698e2cdb279bd
Vasiliy Rusanov <rvk1986@mail.ru>
2022-10-03 15:43:33 +0300

    Переделал урок 3

--- script.js
+++ script.js
@@ -3,24 +3,36 @@
 const title = prompt('Как называется ваш проект?');
 const screens = prompt('Какие типы экранов нужно разработать?');
 const screenPrice = +prompt('Сколько будет стоить данная работа?');
-const rollback = 560;
-let fullPrice = 0;
+const rollback = 56;
 const adaptive = confirm('Нужен ли адаптив на сайте?');
 const service1 = prompt('Какой дополнительный тип услуги нужен?');
 const servicePrice1 = +prompt('Сколько это будет стоить?');
 const service2 = prompt('Какой дополнительный тип услуги нужен?');
 const servicePrice2 = +prompt('Сколько это будет стоить?');
-fullPrice = (screenPrice + servicePrice1 + servicePrice2);
+let fullPrice = (screenPrice + servicePrice1 + servicePrice2);
 const servicePercentPrice = Math.ceil((fullPrice - rollback));
 
 console.log(servicePercentPrice);
 
-if(fullPrice > 30000) {
-    console.log('Вам положена скиидка 10%')
-} else if (fullPrice > 15000 && fullPrice <= 30000) {
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log(`"Стоимость верстки экранов ${screenPrice} долларов" и “Стоимость разработки сайта ${fullPrice})долларов`);
+
+console.log(screens.toLocaleLowerCase().split(', '));
+
+console.log((fullPrice * (rollback/100)));
+
+
+if(fullPrice >= 15000 && fullPrice <= 30000) {
     console.log('Даем скидку в 5%');
-} else if (fullPrice >= 0 && fullPrice <= 15000) {
+} else if (fullPrice >= 0 && fullPrice < 15000) {
     console.log('Скидка не предусмотрена');
-} else if (fullPrice < 0) {
+} else if (fullPrice > 30000) {
+    console.log('Даем скидку в 10%');
+} else {
     console.log('Что то пошло не так');
 }
